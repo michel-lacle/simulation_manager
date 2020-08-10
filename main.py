bf9f43d6e89c149b79bfa590c3e7816369b14956
Michel Lacle <mlacle@progressrail.com>
2020-08-10 10:40:56 -0400

    chore: clean up variable names

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import threading, queue, inprocess_queue_reader_writer
+import threading
 from inprocess_queue_reader_writer import InProcessReaderWriter
 
 
 def start_simulation():
     print("start simulation")
 
-    reader1 = InProcessReaderWriter("reader1")
-    reader2 = InProcessReaderWriter("reader2")
+    reader_writer1 = InProcessReaderWriter("reader1")
+    reader_writer2 = InProcessReaderWriter("reader2")
 
-    threading.Thread(target=reader1.read_queue_messages, daemon=True).start()
-    threading.Thread(target=reader2.read_queue_messages, daemon=True).start()
+    threading.Thread(target=reader_writer1.read_queue_messages, daemon=True).start()
+    threading.Thread(target=reader_writer2.read_queue_messages, daemon=True).start()
 
-    # send thirty task requests to the worker
+    # send thirty task requests to the workers
     for item in range(30):
-        reader1.put_message(str(item))
+        reader_writer1.put_message(str(item))
     print('All task requests sent\n', end='')
 
     # block until all tasks are done
